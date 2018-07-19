30a9259ec00e1c281d0ef0d8801697af5e5c7f0a
Sean Urgel <seantheurgel@gmail.com>
2018-07-19 17:05:09 +0800

    Added padding

--- src/components/Navigation/Sidenav/Sidenav.js
+++ src/components/Navigation/Sidenav/Sidenav.js
@@ -10,9 +10,10 @@ const navigationItems = [
 const Sidenav = (props) => {
 
   const width = props.open ? '200px' : 0;
+  const padding = props.open ? '60px 20px 20px 20px' : 0;
 
   return (
-    <div className="sidenav" style={{height: '100%', width}}>
+    <div className="sidenav" style={{height: '100%', width, padding}}>
       <ul className="list-group list-group-flush">
         {navigationItems.map(name => (
           <NavLink 
