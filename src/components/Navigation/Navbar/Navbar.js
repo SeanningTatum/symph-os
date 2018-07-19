2f5b908a87c8c4ccad578dbd6e280c81d15daa00
Sean Urgel <seantheurgel@gmail.com>
2018-07-19 17:04:38 +0800

    Added  navbar styling

--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Navbar.css';
 
 const navbar = (props) => (
   <nav className="navbar navbar-light bg-light">
-    <span className="navbar-brand mb-0 h1">Symph OS</span>
-    <span onClick={props.clicked}>toggle sidenav</span>
+      <span className="navbar-brand mb-0 h1 pl-2">Symph OS</span>
+      <span className="pr-2" onClick={props.clicked}>toggle sidenav</span>
   </nav>
 )
 
