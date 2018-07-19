d9d2cdaf82ba7eb36d9295c48126976e79e5c9e0
Sean Urgel <seantheurgel@gmail.com>
2018-07-19 14:40:36 +0800

    imported newly created containers to App.js for routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import ClientsPage from 'containers/Clients/Clients';
+import { Switch, Route } from 'react-router';
+
+// HOC
 import Layout from 'hoc/Layout';
 
-import { Switch, Route } from 'react-router';
+// Containers
+import ClientsPage from 'containers/Clients/Clients';
+import ProjectsPage from 'containers/Projects/Projects';
+import ContactsPage from 'containers/Contacts/Contacts';
+import SymphersPage from 'containers/Symphers/Symphers';
+import TeamsPage from 'containers/Teams/Teams';
 
 class App extends Component {
   render() {
     return (
       <Layout>
         <Switch>
-          <Route component={ClientsPage} path='' exact/>
+          <Route component={ClientsPage} path='/clients'/>
+          <Route component={ProjectsPage} path='/projects'/>
+          <Route component={ContactsPage} path='/contacts'/>
+          <Route component={SymphersPage} path='/symphers'/>
+          <Route component={TeamsPage} path='/teams'/>
         </Switch>
       </Layout>
     );
