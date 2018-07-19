8c8527d567fa8b19cb591fa27fd6c8e4699e489c
Sean Urgel <seantheurgel@gmail.com>
2018-07-19 11:25:56 +0800

    Created navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import Navbar from 'components/Navigation/Navbar/Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h1>Hello</h1>
-      </div>
+      <Navbar />
     );
   }
 }
