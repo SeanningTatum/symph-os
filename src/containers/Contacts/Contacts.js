4d56f5ae861e15d85a344e1713d6a72d0917f16c
Sean Urgel <seantheurgel@gmail.com>
2018-07-19 15:42:18 +0800

    Added table

--- src/containers/Contacts/Contacts.js
+++ src/containers/Contacts/Contacts.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
+import BootstrapTable from 'react-bootstrap-table-next';
+
+const columns = [{
+  dataField: 'client_id',
+  text: 'Client ID',
+  sort: true
+}, {
+  dataField: 'client_name',
+  text: 'Name',
+  sort: true
+}, {
+  dataField: 'contact_name',
+  text: 'Contact Name'
+}, {
+  dataField: 'legal_type',
+  text: 'Legal Name'
+}, {
+  dataField: 'Type',
+  text: 'Company'
+}];
+
+const data = [
+  {  client_id: 1, client_name: 'Symph', contact_name: 'Sean Urgel', legal_type: 'Sean Stuart Urgel', Type: 'Company'},
+]
 
 export class Contacts extends Component {
   render() {
     return (
       <div>
-        <h1>Contacts</h1>
+        <h5>Contacts</h5>
+        <BootstrapTable 
+          keyField='client_id' 
+          data={ data } 
+          columns={columns} 
+          bordered={ false }
+          condensed />
       </div>
     )
   }
