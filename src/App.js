5864a93cb1ee4e545f77fd1e5027b6742cb8d725
Sean Urgel <seantheurgel@gmail.com>
2018-07-19 11:35:27 +0800

    Added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import Navbar from 'components/Navigation/Navbar/Navbar';
+import ClientsPage from 'containers/Clients/Clients';
+
+import { Switch, Route } from 'react-router';
 
 class App extends Component {
   render() {
     return (
-      <Navbar />
+      <div>
+        <Navbar />
+
+        <Switch>
+          <Route component={ClientsPage} path='' exact/>
+        </Switch>
+      </div>
     );
   }
 }
---
+++ src/containers/Clients/Clients.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Clients extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Clients Page</h1>
+      </div>
+    )
+  }
+}
+
+export default Clients;
