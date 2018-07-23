960c5a6512e98bdf198bac26d7123a9d0fac6fb2
Sean Urgel <seantheurgel@gmail.com>
2018-07-23 13:05:22 +0800

    created contactControls

---
+++ src/utils/formControls/contactControls.js
@@ -0,0 +1,10 @@
+import { Input } from 'utils/formControls/formModel';
+
+export const contactControls = {
+  contact_name: new Input('input', 'Contact Name', {required: true}),
+  contact_nickname: new Input('input', 'Nickname', {requred: true}),
+  contact_number: new Input('input', 'Contact Number', {required: true, minLength: 11}),
+  contact_email: new Input('input', 'Email', {required: true, isEmail: true}),
+  contact_company: new Input('input', 'Company', {required: true}),
+  contact_position: new Input('input', 'Postiion', {required: true})
+}
