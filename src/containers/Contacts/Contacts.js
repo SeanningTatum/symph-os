99223b474622b425dc154419e44e41eee250a9f9
Sean Urgel <seantheurgel@gmail.com>
2018-07-23 11:27:22 +0800

    added button area

--- src/containers/Contacts/Contacts.js
+++ src/containers/Contacts/Contacts.js
@@ -11,6 +11,9 @@ export class Contacts extends Component {
   /*- - - - - - - - - - - - - - - -
   *           Functions           *
   * - - - - - - - - - - - - - - - */
+  gotoAddContact = () => {
+    this.props.location.push('/contacts/add-contact');
+  }
   /*- - - - - - - - - - - - - - - -
   *             Render            *
   * - - - - - - - - - - - - - - - */
@@ -20,6 +23,12 @@ export class Contacts extends Component {
   render() {
     return (
       <React.Fragment>
+        <div className="button-area">
+          <button className="btn" onClick={this.gotoAddContact}>
+            <i className="material-icons">person_add</i>
+            Add Contact
+          </button>
+        </div>
         <BootstrapTable 
           keyField="contact_id"
           data={[]}
