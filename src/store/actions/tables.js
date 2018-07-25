7b2ccd62c1590b58e0d6393f3a07f2a1eead0bda
Sean Urgel <seantheurgel@gmail.com>
2018-07-25 17:06:34 +0800

    changed functions into try catch finally for readibility

--- src/store/actions/tables.js
+++ src/store/actions/tables.js
@@ -2,14 +2,16 @@ import * as actionTypes from './actionTypes';
 
 const url = "http://localhost:8080/_ah/api/";
 
+/*=============================================
+=                 Start ADD                 =
+=============================================*/
 export const addSuccess = (tableName, data) => ({
   type: actionTypes.ADD_SUCCESS,
   data,
   tableName,
 });
 
-export const add = (tableName, formControls, urlName) => {
-
+export function add (tableName, formControls, api) {
   const data = {};
 
   for (const key in formControls) {
@@ -22,45 +24,47 @@ export const add = (tableName, formControls, urlName) => {
     body: JSON.stringify(data)
   }
 
-  return dispatch => {
-    fetch("http://localhost:8080/_ah/api/contacts-api/v1/create", options)
-      .then(response => response.json())
-      .then(data => {
-        dispatch(addSuccess(tableName, data))
-      })
-      .catch(error => {
-        console.error('INTERNAL SERVER ERROR')
-      });
+  return async dispatch => {
+    try {
+      const response = await fetch(url + `${api}/v1/create`, options);
+      const data = await response.json();
+      dispatch(addSuccess(tableName, data));
+    } catch (error) {
+      console.error(error);
+    } finally {
+
+    }
   }
 };
+/*=====   End of add  ======*/
 
-
+/*=============================================
+=                 Start GET                 =
+=============================================*/
 const getStart = () => ({type: actionTypes.GET_START});
-const getEnd = () => ({type: actionTypes.GET_END})
-
+const getEnd = () => ({type: actionTypes.GET_END});
 const getSuccess = (tableName, dataArray) => ({
   type: actionTypes.GET_SUCCESS,
   tableName,
   dataArray
-})
+});
 
-export const get = (tableName, api) => {
-  return dispatch => {
+export function get(tableName, api) {
+  return async dispatch => {
     dispatch(getStart());
-    console.log('starting')
-    setTimeout(() => dispatch(getEnd()), 5000)
 
-    // fetch(url + `${api}/v1/get`)
-    //   .then(response => response.json())
-    //   .then(dataArray => {
-    //     dispatch(getSuccess(tableName, dataArray[tableName]))
-    //     dispatch(getEnd())
-    //   })
-    //   .catch(error => {
-    //     console.error('INTERNAL SERVER ERROR')
-    //     getEnd();
-    //   });
+    try {
+      const response = await fetch(url + `${api}/v1/get`);
+      const dataArray = await response.json();
+      dispatch(getSuccess(tableName, dataArray[tableName]));
+    } catch (error) {
+      console.error(error);
+    } finally {
+      dispatch(getEnd());
+    }
   }
-
 }
+/*=====   End of get  ======*/
+
+
 
