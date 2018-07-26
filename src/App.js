f01dddc2b496dff171c4471eff00d8a2b8d8e774
Sean Urgel <seantheurgel@gmail.com>
2018-07-26 22:28:08 +0800

    Added Google Login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import { GoogleLogin } from 'react-google-login';
 // HOC
 import Layout from 'hoc/Layout/Layout';
 
@@ -28,28 +28,37 @@ import TeamsPage from 'containers/Teams/Teams';
 /*=====  End of containers  ======*/
 
 class App extends Component {
+  state = {
+    isLoggedIn: true
+  }
 
   render() {
     return (
-      <Layout>
-        <Switch>
-            <Route path='/contacts/add-contact' component={AddContactPage} exact/>
-            <Route path='/contacts/:id' component={ContactProfile}/>
-            <Route path='/contacts' component={ContactsPage} exact />
+      <React.Fragment>
+        {this.state.isLoggedIn ? (
+          <Layout>
+            <Switch>
+                <Route path='/contacts/add-contact' component={AddContactPage} exact/>
+                <Route path='/contacts/:id' component={ContactProfile}/>
+                <Route path='/contacts' component={ContactsPage} exact />
 
-            <Route path='/projects' component={ProjectsPage} exact />
+                <Route path='/projects' component={ProjectsPage} exact />
 
-            <Route path='/clients/add-client' component={AddClientPage} exact />
-            <Route path='/clients/:id' component={ClientProfilePage} />          
-            <Route path='/clients' component={ClientsPage} exact />
+                <Route path='/clients/add-client' component={AddClientPage} exact />
+                <Route path='/clients/:id' component={ClientProfilePage} />          
+                <Route path='/clients' component={ClientsPage} exact />
 
-            <Route path='/employees/add-employee' component={AddEmployeesPage} />
-            <Route path='/employees' component={EmployeesPage} exact />
+                <Route path='/employees/add-employee' component={AddEmployeesPage} />
+                <Route path='/employees' component={EmployeesPage} exact />
 
-            <Route path='/teams' component={TeamsPage} exact />
-            <Route render={() => <h2> NOT FOUND </h2>} />
-        </Switch>
-      </Layout>
+                <Route path='/teams' component={TeamsPage} exact />
+                <Route render={() => <h2> NOT FOUND </h2>} />
+            </Switch>
+          </Layout>
+          ) : (
+            <Route render={() => <GoogleLogin />} />
+          )}
+      </React.Fragment>
     );
   }
 }
