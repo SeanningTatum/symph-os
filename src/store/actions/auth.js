a4f12cc29c436c4781356d13a72f0dd025e639ae
Sean Urgel <seantheurgel@gmail.com>
2018-07-30 09:52:15 +0800

    did some testing with refresh tokens

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -24,11 +24,14 @@ export function onAuth (response) {
       return;
     }
 
+    // response.reloadAuthResponse()
+
     const {access_token, expires_in} = response.Zi;
 
     const extractProfile = ({ email, name, imageUrl }) => ({ email, name, imageUrl });
     const profile = extractProfile(response.profileObj);
 
+    localStorage.setItem('response', JSON.stringify(response));
     localStorage.setItem('token', access_token);
     localStorage.setItem('profile', JSON.stringify(profile))
     localStorage.setItem('expiration_date', new Date(new Date().getTime() + expires_in * 1000));
@@ -42,16 +45,18 @@ export function isLoggedIn() {
     const token = localStorage.getItem('token');
     if (!token) {
       dispatch(logout());
-    } else {
-      const expirationDate = new Date(localStorage.getItem('expiration_date'));
-      console.log(expirationDate);
+      return;
+    } 
 
-      if (expirationDate <= new Date()) {
-        dispatch(logout());
-      } else {
-        const profile = JSON.parse(localStorage.getItem('profile'));
-        dispatch(onAuthSuccess(profile, token));
-      }
+    const expirationDate = new Date(localStorage.getItem('expiration_date'));
+    console.log(expirationDate);
+      
+    if (expirationDate <= new Date()) {
+      dispatch(logout());
+    } else {
+      const profile = JSON.parse(localStorage.getItem('profile'));
+      dispatch(onAuthSuccess(profile, token));
     }
+  
   }
 }
