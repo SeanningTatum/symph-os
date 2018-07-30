e02e1fc797c6539926603c072689f61a8f066a0c
Sean Urgel <seantheurgel@gmail.com>
2018-07-30 16:25:08 +0800

    changed client_name to name

--- src/utils/formControls/clientControls.js
+++ src/utils/formControls/clientControls.js
@@ -3,7 +3,7 @@ import {Input, Select} from 'utils/formControls/formModel';
 const options = ["Non Profit Organization","Individual","Government","Startup","School","Company"];
 
 export const clientControls = {
-  client_name: new Input('input', 'Client Name*', {required: true, minLength: 3}), 
+  name: new Input('input', 'Client Name*', {required: true, minLength: 3}), 
   legal_name: new Input('input', 'Legal Name*', {required: true}),
   type: new Select('select', 'Type*', {required: true}, options),
 }
