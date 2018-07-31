dc48e12491066758b73e3d3ea1201a22b7bb91cd
Sean Urgel <seantheurgel@gmail.com>
2018-07-31 18:28:36 +0800

    card now shows values

--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 import './Card.scss';
 
-const Card = () => (
+const Card = ({leader, name, project_manager, slack}) => (
   <div className="card" style={{textAlign: 'center'}}>
     <div className="card-header">
-      Team Firebaes
+      {name}
     </div>
     <div className="card-body">
-      <h5 className="card-title"><strong>Project Manager: Joshua Ty</strong></h5>
-      <h5 className="card-text">Team Leader: Vince Villahermosa</h5>
+      <h5 className="card-title"><strong>Project Manager: {project_manager}</strong></h5>
+      <h5 className="card-text">Team Leader: {leader}</h5>
+      <h5 className="card-text">Slack: {slack}</h5>
       <button className="btn btn-primary" style={{alignSelf: 'center'}}>View Projects</button>
     </div>
     <div className="card-footer text-muted">
@@ -16,5 +18,12 @@ const Card = () => (
     </div>
   </div>
 )
+
+Card.propTypes = {
+  leader: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  project_manager: PropTypes.string.isRequired,
+  slack: PropTypes.string.isRequired,
+}
 
 export default Card
