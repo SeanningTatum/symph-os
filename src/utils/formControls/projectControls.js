27ee8304d6ced4db2d8962273bd47513897afc98
Sean Urgel <seantheurgel@gmail.com>
2018-07-31 20:56:44 +0800

    removed controls, because they're now selects

--- src/utils/formControls/projectControls.js
+++ src/utils/formControls/projectControls.js
@@ -2,9 +2,5 @@ import { Input } from './formModel';
 
 export const projectControls = {
   project_name: new Input('input', 'Project Name*', {required: true}),
-  client: new Input('input', 'Client*', {required: true}),
-  client_contact: new Input('input', 'Client Contact*', {required: true}),
-  project_manager: new Input('input', 'Project Manager*', {required: true}),
-  team: new Input('input', 'Team*', {required: true}),
   total_no_of_budgeted_blocks: new Input('input', 'Total # of Budgeted Blocks*', {required: true})
 }
