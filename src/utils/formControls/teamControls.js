4753198ba0e0813a48d4fa8e53a1833fc54c9b35
Sean Urgel <seantheurgel@gmail.com>
2018-07-31 18:17:45 +0800

    changed names to match vinces

--- src/utils/formControls/teamControls.js
+++ src/utils/formControls/teamControls.js
@@ -1,8 +1,8 @@
 import { Input } from './formModel';
 
 export const teamControls = {
-  team_name: new Input('input', 'Team Name*', {required: true}),
+  name: new Input('input', 'Team Name*', {required: true}),
   project_manager: new Input('input', 'Project Manager*', {required: true}),
-  team_leader: new Input('input', 'Team Leader*', {required: true}),
-  team_slack_channel: new Input('input', 'Team Slack Channel', {required: false})
+  leader: new Input('input', 'Team Leader*', {required: true}),
+  slack: new Input('input', 'Team Slack Channel', {required: false})
 }
