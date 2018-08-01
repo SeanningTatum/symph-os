2b5bcc95b5b42a4a3ea296d690627a7959c34e79
Sean Urgel <seantheurgel@gmail.com>
2018-08-01 09:05:43 +0800

    removed unecessary controls

--- src/utils/formControls/teamControls.js
+++ src/utils/formControls/teamControls.js
@@ -2,7 +2,5 @@ import { Input } from './formModel';
 
 export const teamControls = {
   name: new Input('input', 'Team Name*', {required: true}),
-  project_manager: new Input('input', 'Project Manager*', {required: true}),
-  leader: new Input('input', 'Team Leader*', {required: true}),
   slack: new Input('input', 'Team Slack Channel', {required: false}),
 }
