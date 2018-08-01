59fa875487b8a7945a6bcb15f29c2f29df8ae3c8
Sean Urgel <seantheurgel@gmail.com>
2018-08-01 09:05:24 +0800

    toString the value so it always shows

--- src/components/Profile/ProfileDetails/ProfileDetails.js
+++ src/components/Profile/ProfileDetails/ProfileDetails.js
@@ -5,7 +5,7 @@ const ProfileDetails = props => {
   return props.profile.map(p => (
     <div key={p.id} className="profile--info-area__info">
       <h3 className="info__name">{p.id}</h3>
-      <p className="info__value">{p.value}</p>
+      <p className="info__value">{p.value.toString()}</p>
     </div>
   ))
 }
