51d140fd81261783855c3efed75683f1fa27f4ff
Sean Urgel <seantheurgel@gmail.com>
2018-08-01 17:33:19 +0800

    removed underscores and replace it with space

--- src/components/Profile/ProfileDetails/ProfileDetails.js
+++ src/components/Profile/ProfileDetails/ProfileDetails.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const ProfileDetails = props => {
   return props.profile.map(p => (
     <div key={p.id} className="profile--info-area__info">
-      <h3 className="info__name">{p.id}</h3>
+      <h3 className="info__name">{p.id.replace(/[_]/g, " ")}</h3>
       <p className="info__value">{p.value.toString()}</p>
     </div>
   ))
