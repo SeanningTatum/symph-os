aa95572f6f2c90dcb313ec9f1dcec62f1b918139
Sean Urgel <seantheurgel@gmail.com>
2018-08-02 10:05:16 +0800

    Changed profile details to contain a side nav

--- src/components/Profile/ProfileDetails/ProfileDetails.js
+++ src/components/Profile/ProfileDetails/ProfileDetails.js
@@ -1,13 +1,36 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+
+// const ProfileDetails = props => {
+//   return props.profile.map(p => (
+//     <div key={p.id} className="profile--info-area__info">
+//       <h3 className="info__name">{p.id.replace(/[_]/g, " ")}</h3>
+//       <p className="info__value">{p.value.toString()}</p>
+//     </div>
+//   ))
+// }
 
 const ProfileDetails = props => {
-  return props.profile.map(p => (
-    <div key={p.id} className="profile--info-area__info">
-      <h3 className="info__name">{p.id.replace(/[_]/g, " ")}</h3>
-      <p className="info__value">{p.value.toString()}</p>
-    </div>
-  ))
+  return (
+    <React.Fragment>
+      <aside className="profile--info-area__navigation">
+        <div className="inner">
+          <section>
+            <div className="section-heading">
+              <strong>PROFILE</strong>
+            </div>
+            <ul>
+              <li><NavLink to='/'>Basic</NavLink></li>
+              <li>Employment</li>
+              <li>Government Membership</li>
+              <li>Personal and Family</li>
+            </ul>
+          </section>
+        </div>
+      </aside>
+    </React.Fragment>
+  );
 }
 
 ProfileDetails.propTypes = {
