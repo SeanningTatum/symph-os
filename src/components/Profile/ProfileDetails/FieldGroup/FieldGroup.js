99ddc4c3ad4850741e134ae10f9792aa0d989e5d
Sean Urgel <seantheurgel@gmail.com>
2018-08-02 15:42:20 +0800

    change input type based on given elementType

--- src/components/Profile/ProfileDetails/FieldGroup/FieldGroup.js
+++ src/components/Profile/ProfileDetails/FieldGroup/FieldGroup.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FieldGroup = props => (
-  <div className="field-group">
-    <div className="label">
-      <label>{props.label}</label>
+const FieldGroup = props => {
+
+  let input;
+
+  switch(props.elementType) {
+    case 'input': input = <input value={props.value} type="input"/>; break;
+    case 'textarea': input = <textarea value={props.value} type="input" rows="3" />; break;
+
+    default: input = <input value={props.value} type="input"/>; break;
+  }
+
+
+  return (
+    <div className="field-group">
+      <div className="label">
+        <label>{props.label}</label>
+      </div>
+      {props.edit ? (
+        <p>{props.value || 'none'}</p>
+      ) : (
+        input
+      )}
     </div>
-    <p>{props.value || 'none'}</p>
-  </div>
-)
+  )
+}
 
 FieldGroup.propTypes = {
   label: PropTypes.string.isRequired,
-  value: PropTypes.string
+  value: PropTypes.string,
+  edit: PropTypes.bool.isRequired,
+  elementType: PropTypes.string.isRequired
 }
 
 export default FieldGroup
