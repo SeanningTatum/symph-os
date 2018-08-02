d174a8c6b139af46067b151b9f8c4db56ac3128a
Sean Urgel <seantheurgel@gmail.com>
2018-08-02 12:48:02 +0800

    properly handles redirects and created employment page

--- src/containers/Employees/EmployeeProfile/EmployeeProfile.js
+++ src/containers/Employees/EmployeeProfile/EmployeeProfile.js
@@ -50,19 +50,15 @@ export class EmployeeProfile extends Component {
   render() {
     console.log(this.props.employeeProfile);
     const {fname, lname, nickname, email, contact_number} = this.props.employeeProfile;
-    const {path} = this.props.match;
+    const {url} = this.props.match;
+    console.log(this.props)
     return (!this.props.loading) ? (
-
-
       <React.Fragment>
-        <ProfileHeader 
-            clicked={this.toggleEdit} 
-            name={`${fname} ${lname}`}
-            edit={this.state.edit}/>
-        <ProfileDetails>
+        <ProfileHeader clicked={this.toggleEdit} name={`${fname} ${lname}`}edit={this.state.edit}/>
+        <ProfileDetails url={url}>
           <h3>General Info</h3>
           <Switch>
-            <Route path={`${path}/general-info`} render={() => (
+            <Route path={`${url}/general-info`} render={() => (
               <React.Fragment>
                 <FieldGroup value={fname} label="First name"/>
                 <FieldGroup value={lname} label="Last name"/>        
@@ -71,7 +67,22 @@ export class EmployeeProfile extends Component {
                 <FieldGroup value={contact_number} label="Contact Number"/>
               </React.Fragment>
             )} />
-            <Route render={() => <Redirect to={`${path}/general-info`}/>}/>
+            <Route path={`${url}/employment`} render={() => (
+              <React.Fragment>
+                <FieldGroup value={contact_number} label="Position"/>
+                <FieldGroup value={contact_number} label="Working Arrangement"/>        
+                <FieldGroup value={contact_number} label="Team"/>
+                <FieldGroup value={contact_number} label="Email"/>
+                <FieldGroup value={contact_number} label="Employment Status"/>
+                <FieldGroup value={contact_number} label="Started Working at Symph"/>
+                <FieldGroup value={contact_number} label="Last Day at Symph"/>
+                <FieldGroup value={contact_number} label="Symph ID no."/>
+                <FieldGroup value={contact_number} label="Employee Number"/>
+                <FieldGroup value={contact_number} label="Title / Tagline / Motto"/>
+                <FieldGroup value={contact_number} label="BPI Payroll Account Number"/>
+              </React.Fragment>
+            )} />
+            <Route render={() => <Redirect to={`${url}/general-info`}/>}/>
           </Switch>
         </ProfileDetails>
       </React.Fragment>
