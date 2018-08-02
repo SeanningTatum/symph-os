63b1b555f9b4c69f4a9d46cb1352889348139445
Sean Urgel <seantheurgel@gmail.com>
2018-08-02 15:43:09 +0800

    created a generalInfo controls in employee and created inputChangedHandler

--- src/containers/Employees/EmployeeProfile/EmployeeProfile.js
+++ src/containers/Employees/EmployeeProfile/EmployeeProfile.js
@@ -14,7 +14,8 @@ import * as profileActions from 'store/actions/profiles';
 export class EmployeeProfile extends Component {
 
   state = {
-    edit: false
+    edit: false,
+    generalInfo: []
   }
 
   /*- - - - - - - - - - - - - - - -
@@ -24,6 +25,7 @@ export class EmployeeProfile extends Component {
   async componentDidMount() {
     const employeeID = this.props.location.pathname.split("/")[2];
     await this.props.get('employees-api', employeeID);
+    this.initGeneralInfo();
   }
 
   componentWillUnmount() {
@@ -32,6 +34,28 @@ export class EmployeeProfile extends Component {
   /*- - - - - - - - - - - - - - - -
   *           Functions           *
   * - - - - - - - - - - - - - - - */
+
+  initGeneralInfo = () => {
+    const { 
+      fname, lname, nickname, email, contact_number, 
+      current_skills, future_skills,personality_type } = this.props.employeeProfile;
+
+    this.setState({generalInfo: [
+        {value: fname, label: "First Name", elementType: 'input'},
+        {value: lname, label: "Last Name", elementType: 'input'},
+        {value: nickname, label: "Nickname", elementType: 'input'},
+        {value: email, label: 'Email', elementType: 'input'},
+        {value: contact_number, label: 'Contact Number', elementType: 'input'},
+        {value: current_skills, label: 'Current Skills and Technologies', elementType: 'textarea'},
+        {value: future_skills, label: 'Skills and Technologies I want to learn in the next 6 months', elementType: 'input'},
+        {value: personality_type, label: 'Personality Type', elementType: 'input'}
+    ]});
+  }
+
+  onInputChangeHandler = (arrayName, label, event) => {
+    const result = this.state[arrayName].find(control => control.label === label);
+    result.value = event.target.value;
+  }
 
   toggleEdit = () => {
     this.setState(prevState => ({ edit: !prevState.edit }));
@@ -48,9 +72,8 @@ export class EmployeeProfile extends Component {
   * - - - - - - - - - - - - - - - */
 
   render() {
-    console.log(this.props.employeeProfile);
-    const { fname, lname, nickname, email, contact_number } = this.props.employeeProfile;
     const { url } = this.props.match;
+    const { fname, lname } = this.props.employeeProfile;
 
     return (!this.props.loading) ? (
       <React.Fragment>
@@ -59,48 +82,39 @@ export class EmployeeProfile extends Component {
           <h3>General Info</h3>
           <Switch>
             <Route path={`${url}/general-info`} render={() => (
-              <React.Fragment>
-                <FieldGroup value={fname} label="First name" />
-                <FieldGroup value={lname} label="Last name" />
-                <FieldGroup value={nickname} label="Nickname" />
-                <FieldGroup value={email} label="Email" />
-                <FieldGroup value={contact_number} label="Current Skills and Technologies" />
-                <FieldGroup 
-                  value={contact_number} 
-                  label="Skills and Technologies I want to learn in the next 6 months" 
-                  />
-                <FieldGroup value={contact_number} label="Personality Type" />
-              </React.Fragment>
+              this.state.generalInfo.map(info => (
+                <FieldGroup {...info} edit={this.state.edit} key={info.value} onChange={this.onInputChangeHandler}/>
+              ))
             )} />
             <Route path={`${url}/employment`} render={() => (
               <React.Fragment>
-                <FieldGroup value={contact_number} label="Position" />
-                <FieldGroup value={contact_number} label="Working Arrangement" />
-                <FieldGroup value={contact_number} label="Team" />
-                <FieldGroup value={contact_number} label="Email" />
-                <FieldGroup value={contact_number} label="Employment Status" />
-                <FieldGroup value={contact_number} label="Started Working at Symph" />
-                <FieldGroup value={contact_number} label="Last Day at Symph" />
-                <FieldGroup value={contact_number} label="Symph ID no." />
-                <FieldGroup value={contact_number} label="Employee Number" />
-                <FieldGroup value={contact_number} label="Title / Tagline / Motto" />
-                <FieldGroup value={contact_number} label="BPI Payroll Account Number" />
+                {/* <FieldGroup value={contact_number} label="Position" /> */}
+                {/* <FieldGroup value={contact_number} label="Working Arrangement" /> */}
+                {/* <FieldGroup value={contact_number} label="Team" /> */}
+                {/* <FieldGroup value={contact_number} label="Email" /> */}
+                {/* <FieldGroup value={contact_number} label="Employment Status" /> */}
+                {/* <FieldGroup value={contact_number} label="Started Working at Symph" /> */}
+                {/* <FieldGroup value={contact_number} label="Last Day at Symph" /> */}
+                {/* <FieldGroup value={contact_number} label="Symph ID no." /> */}
+                {/* <FieldGroup value={contact_number} label="Employee Number" /> */}
+                {/* <FieldGroup value={contact_number} label="Title / Tagline / Motto" /> */}
+                {/* <FieldGroup value={contact_number} label="BPI Payroll Account Number" /> */}
               </React.Fragment>
             )} />
             <Route path={`${url}/government-membership`} render={() => (
               <React.Fragment>
-                <FieldGroup value={contact_number} label="SSS" />
-                <FieldGroup value={contact_number} label="TIN" />
-                <FieldGroup value={contact_number} label="PhilHealth" />
-                <FieldGroup value={contact_number} label="Pagibig" />
+                {/* <FieldGroup value={contact_number} label="SSS" /> */}
+                {/* <FieldGroup value={contact_number} label="TIN" /> */}
+                {/* <FieldGroup value={contact_number} label="PhilHealth" /> */}
+                {/* <FieldGroup value={contact_number} label="Pagibig" /> */}
               </React.Fragment>
             )} />
              <Route path={`${url}/personal-and-family`} render={() => (
               <React.Fragment>
-                <FieldGroup value={contact_number} label="Residence Address"/>
-                <FieldGroup value={contact_number} label="Civil Status"/>        
-                <FieldGroup value={contact_number} label="Spouses Complete Name"/>
-                <FieldGroup value={contact_number} label="Children's Complete Name"/>
+                {/* <FieldGroup value={contact_number} label="Residence Address"/> */}
+                {/* <FieldGroup value={contact_number} label="Civil Status"/>         */}
+                {/* <FieldGroup value={contact_number} label="Spouses Complete Name"/> */}
+                {/* <FieldGroup value={contact_number} label="Children's Complete Name"/> */}
               </React.Fragment>
             )} />
             <Route render={() => <Redirect to={`${url}/general-info`} />} />
