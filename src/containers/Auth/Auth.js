c4d1d00a1df56a50b63e0a3674dd788b50177151
Sean Urgel <seantheurgel@gmail.com>
2018-08-06 11:22:22 +0800

    properly merged

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,11 +6,7 @@ import * as authActions from 'store/actions/auth';
 
 export class Auth extends Component {
   responseGoogle = (response) => {
-<<<<<<< HEAD
     fetch('https://symph-operating-system-210700.appspot.com/_ah/api/credentials/v1/get')
-=======
-    fetch('http://symph-operating-system-210700.appspot.com/_ah/api/credentials/v1/get')
->>>>>>> change-css
       .then(response => response.json())
       .then(whitelist => {
         console.log(whitelist);
